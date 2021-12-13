957bac0d9289feb7efefe63ad7405a151d3b146a
jylee <jylee@175.124.104.161>
2021-12-13 13:14:47 +0900

    boardcontroller test.do

--- ZZTest/src/main/java/com/java/controller/BoardController.java
+++ ZZTest/src/main/java/com/java/controller/BoardController.java
@@ -1,5 +1,14 @@
 package com.java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BoardController {
 
+	
+	@RequestMapping("/test.do")
+	public void test() {
+		System.out.println("/test.do 확인");
+	}
 }
